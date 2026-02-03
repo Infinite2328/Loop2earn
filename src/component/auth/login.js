48b1f11d8c9820fa30918bb9fa33c397e61ef573
Planet <infinite23281@outlook.com>
2026-02-03 04:40:48 -0800

    bacground upgrade

--- src/component/auth/login.js
+++ src/component/auth/login.js
@@ -5,13 +5,36 @@ import log_const from './constant/login';
 import CustomInput from '../utils/input';
 import Button from '../utils/button';
 import { FaGoogle, FaTwitter } from 'react-icons/fa6';
+import firefox from '../../assets/imgs/metamask-icon logo.png';
+import { useEffect, useState } from 'react';
+// import smallLogo from '../../../images/logo/logo2.png';
+import vector from '../../assets/imgs/Vector.png';
 
 function Login() {
+  const [degrees, setDeg] = useState([]);
+  useEffect(() => {
+    var array = [];
+    for (let i = -75; i < 0; i += 3) {
+      array.push(i);
+    }
+    setDeg(array);
+  }, []);
   return (
     <div className="login">
       <div>
         <Navbar />
         <div className="logo">
+          {degrees.map((degree, idx) => (
+            <img
+              src={vector}
+              alt="vector"
+              key={idx}
+              style={{
+                transform: `rotateX(${degree}deg) rotateY(${degree - 10}deg)`,
+                opacity: `${-degree / 75}`
+              }}
+            />
+          ))}
           <img src={Loop} />
         </div>
         <div className="greeting">{log_const.Greeting}</div>
@@ -38,7 +61,9 @@ function Login() {
           <div className="utils-sites">
             <FaGoogle size={35} />
           </div>
-          <div className="utils-sites"></div>
+          <div className="utils-sites">
+            <img src={firefox} />
+          </div>
           <div className="utils-sites">
             <FaTwitter size={35} />
           </div>
